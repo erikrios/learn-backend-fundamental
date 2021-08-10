c9e3eb7682e906f7a8b396423a180220d0f4b43a
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-08-11 01:49:47 +0700

    Create 1628620811847_create-collaborations-table.js

--- notes-app-back-end/migrations/1628620811847_create-collaborations-table.js
+++ notes-app-back-end/migrations/1628620811847_create-collaborations-table.js
@@ -23,8 +23,8 @@ exports.up = (pgm) => {
   pgm.addConstraint('collaborations', 'unique_note_id_and_user_id', 'UNIQUE(note_id, user_id)');
 
   // Add the foreign key
-  pgm.addConstraint('collaborations', 'fk_collaborations.note_id_notes.id', 'FOREIGN KEY(note_id_ REFERENCES notes(id) ON DELETE CASCADE');
-  pgm.addConstraint('collaborations', 'fk_collaborations.user_id_users.id', 'FOREIGN KEY(user_id_ REFERENCES users(id) ON DELETE CASCADE');
+  pgm.addConstraint('collaborations', 'fk_collaborations.note_id_notes.id', 'FOREIGN KEY(note_id) REFERENCES notes(id) ON DELETE CASCADE');
+  pgm.addConstraint('collaborations', 'fk_collaborations.user_id_users.id', 'FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE');
 };
 
 exports.down = (pgm) => {
