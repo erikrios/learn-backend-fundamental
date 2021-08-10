0294243c2c986c40db7c950f465b8c3713dbe9a9
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-08-11 02:09:29 +0700

    Create addCollaboration function

--- notes-app-back-end/src/services/postgres/CollaborationsService.js
+++ notes-app-back-end/src/services/postgres/CollaborationsService.js
@@ -1,8 +1,27 @@
+const { nanoid } = require('nanoid');
 const { Pool } = require('pg');
+const InvariantError = require('../../exceptions/InvariantError');
 
 class CollaborationsService {
   constructor() {
     this._pool = new Pool();
+  }
+
+  async addCollaboration(noteId, userId) {
+    const id = `collab-${nanoid(16)}`;
+
+    const query = {
+      text: 'INSERT INTO collaborations VALUES($1, $2, $3) RETURNING id',
+      values: [id, noteId, userId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new InvariantError('Kolaborasi gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
   }
 }
 
